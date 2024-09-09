6e53b55b83f135a6a283e1864d6f80dc333cf5ff
JoeBreadKing <117707033+JoeBreadKing@users.noreply.github.com>
2024-09-10 00:22:48 +0900

    responsive

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -1,47 +1,32 @@
 import React from "react";
-import {Link} from "react-router-dom";
-import HeadImg from "../images/part/head.png";
-import Exercise from "../images/button_img/Exercise.png";
-import FAQ from "../images/button_img/FAQ.png";
-import MyGoal from "../images/button_img/myGoal.png";
-import SelfDiagnose from "../images/button_img/selfDiagnose.png";
-import NavBar from "../images/part/foot.png";
+// import {Link} from "react-router-dom";
+// import HeadImg from "../images/part/head.png";
+// import Exercise from "../images/button_img/Exercise.png";
+// import FAQ from "../images/button_img/FAQ.png";
+// import MyGoal from "../images/button_img/myGoal.png";
+// import SelfDiagnose from "../images/button_img/selfDiagnose.png";
+// import NavBar from "../images/part/foot.png";
 import styles from "./routes_css/Main.module.css";
 
 function Main(){
     return(
-        <div className={styles.container}>
-            <div className={styles.headContainer}>
-                <img className={styles.headImg} src={HeadImg} />
-            </div>
-            <div className={styles.greeting}>
-                <h1 id="userName">김정호</h1>
-                <h2>님 오늘도 건강한 하루 보내세요!</h2>
-            </div>
-            <div className={styles.selectContainer}>
-                <Link to={'../routes/SelfDiagnose'} className={styles.mainBtn}>
-                    <button className={styles.mainBtn}>
-                        <img id="SelfDiagnose" src={SelfDiagnose} />
-                    </button>
-                </Link>
-                <Link to={'../routes/SelfDiagnose'} className={styles.mainBtn}>
-                    <button className={styles.mainBtn}>
-                        <img id="Exercise" src={Exercise}/>
-                    </button>
-                </Link>
-                <Link to={'../routes/SelfDiagnose'} className={styles.mainBtn}>
-                    <button className={styles.mainBtn}>
-                        <img id="MyGoal" src={MyGoal}/>
-                    </button>
-                </Link>
-                <Link to={'../routes/SelfDiagnose'} className={styles.mainBtn}>
-                    <button className={styles.mainBtn}>
-                        <img id="FAQ" src={FAQ}/>
-                    </button>
-                </Link>
-            </div>
-            <div className={styles.navBar}>
-                <img src={NavBar} />
+        <div id={styles.wrapper}>
+            <div id={styles.container}>
+                <div className={styles.header}>
+                    <h1 id="mainTheme">닥터마법사</h1>
+                </div>
+                <div className={styles.greeting}>
+                    <h1 id="userName">김정호</h1>
+                    <h2>님 오늘도 건강한 하루 보내세요!</h2>
+                </div>
+                <div className={styles.selectContainer}>
+                    <div className={styles.select}>
+                        <div className={styles.div1}> </div>
+                        <div className={styles.div2}> </div>
+                        <div className={styles.div3}> </div>
+                        <div className={styles.div4}> </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
